773a2eb77f88e21d1867f22907c5c2edaa55aea9
Tristan B. Kildaire <deavmi@redxen.eu>
2022-01-16 18:12:10 +0200

    Added Tasky Engine initialization (partly), initialize the Eventy engine, implemented Descriptor registration

--- source/tasky/engine.d
+++ source/tasky/engine.d
@@ -7,9 +7,29 @@
 */
 module tasky.engine;
 
+import eventy.engine : EvEngine = Engine;
+import tasky.jobs : Descriptor;
+
 public final class Engine
 {
+	private EvEngine evEngine;
+
 	this()
 	{
+		/* Create a new event engine */
+		evEngine = new EvEngine();
+		evEngine.start();
+	}
+
+	/**
+	* Register a Descriptor with tasky
+	*/
+	public void registerDescriptor(Descriptor desc)
+	{
+		/* Add a queue based on the descriptor ID */
+		evEngine.addQueue(desc.getDescriptorClass());
+
+		/* Add a signal handler that handles said descriptor ID */
+		evEngine.addSignalHandler(desc);
 	}
 }
